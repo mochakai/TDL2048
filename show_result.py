b31a0b2b652fd5b505c3ade414e63967104d756e
KAI_desktop <ms0346648@gmail.com>
2019-05-25 22:01:46 +0800

    - add show result

---
+++ show_result.py
@@ -0,0 +1,33 @@
+import sys
+from argparse import ArgumentParser
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def show_result(res):
+    plot = res.plot(title='2048 average score')
+    plot.set_xlabel("per 1000")
+    plot.set_ylabel("avg score")
+    
+    plt.show()
+
+
+def main(args):
+    source1 = pd.read_csv(args.file_name1, index_col=0, names=["idx", "before_state"])
+    source2 = pd.read_csv(args.file_name2, index_col=0, names=["idx", "after_state"])
+    source = pd.concat([source1, source2], axis=1, sort=False)
+    show_result(source)
+
+
+def get_args():
+    parser = ArgumentParser()
+    parser.add_argument("file_name1", help="your csv file name", type=str, default='')
+    parser.add_argument("file_name2", help="your csv file name", type=str, default='')
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    main(get_args())
+
+    sys.stdout.flush()
+    sys.exit()
